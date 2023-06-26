3c693bf98f52e2c2a19948385f0fbe4c05a487a9
elluis <elluis.codes@gmail.com>
2023-06-26 05:12:39 -0500

    cleaning up our darkmode implementation - about to build out the button.tsx using shadcn library - heavily modified to use next.js features - ie. <Link/>@next.js

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { AnimatePresence } from "framer-motion";
 import ThemeToggle from "@/components/theme/theme-toggle";
 
 const Header = () => {
   return (
-    <div className="w-screen min-h-[200px] sticky top-0 left-0 right-0 inset-0 bg-red-300">
-      <ThemeToggle />
-    </div>
+    <AnimatePresence>
+      <div className="w-screen min-h-[200px] sticky top-0 left-0 right-0 inset-0 bg-red-300">
+        <ThemeToggle />
+      </div>
+    </AnimatePresence>
   );
 };
 export default Header;
